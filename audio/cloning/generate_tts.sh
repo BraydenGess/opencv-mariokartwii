35765f6a840f9df57ecfb18c8bb289b47a936137
bradygess <bradygess13@gmail.com>
2025-02-23 09:33:59 -0500

    update gitignore

--- audio/cloning/generate_tts.sh
+++ audio/cloning/generate_tts.sh
@@ -3,8 +3,9 @@
 # Leave --ref_text "" will have ASR model transcribe (extra GPU memory usage)
 f5-tts_infer-cli \
 --model "F5-TTS" \
---ref_audio "audio/cloning/audio_GusJohnson01-01.wav" \
---ref_text "That place is loud and it's so aesthetically beautiful. Your right there on
-the water, and washington has those cool uniforms with orange I mean uh a purple,
-black and yellow" \
---gen_text "Thanks Jenny! Snow is always fun for the fans but how will it impact today's race?"
+--ref_audio "audio/cloning/audio_JoelKlatt01-01.wav" \
+--ref_text "One more player left on the offense that can go out for a passing route eligibility which is this running back
+right here this running back is going to be covered in tandem by the two linebackers based on where he releases" \
+--gen_text "Yes, I really like the 2 on 2 style because it adds another dimension to the race.
+In order to win today both teammates are going to have to perform at a high level. One teammate is going to be worse, that's just the reality of the situation.
+Today is not just about drinking and driving but also dealing with adversity and team chemistry"
