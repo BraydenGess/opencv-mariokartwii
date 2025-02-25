687d03ebc340722d3689e95e89048c3e9f08f009
bradygess <bradygess13@gmail.com>
2025-02-24 22:17:39 -0500

    Typing updates

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-import random
-import time
 import os
-from collections import deque
+import time
+import random
 import spotipy
+from typing import Dict, List, Optional
+from collections import deque
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 from development.course_detection.course_detection import CourseDetector
 
@@ -14,6 +15,17 @@ class ModelStore():
     def load_models(self):
         self.models['course_detector'] = CourseDetector(flag_model_path='development/course_detection/models/flag_detector_20250221_122742.pth')
 
+class Song():
+    def __init__(self,song_name,uri,img):
+        self.song_name = song_name
+        self.uri = uri
+        self.img = img
+
+
+class Course():
+    def __init__(self, course_name = None, song_queue = None):
+        self.course_name = course_name
+        self.song_queue = song_queue
 
 class SpotifyPlayer():
     def __init__(self):
@@ -32,7 +44,7 @@ class SpotifyPlayer():
         if self.support_volume:
             self.spotify.volume(volume_percent = 100, device_id = None)
 
-    def skip_tosong(self, song_uri):
+    def skip_tosong(self, song_uri: str):
         self.min_volume()
         user_queue = self.spotify.queue()['queue']
         time.sleep(0.1)
@@ -43,25 +55,25 @@ class SpotifyPlayer():
                 break
         self.max_volume()
 
-    def queue_songs(self,songs):
+    def queue_songs(self, songs: List[Song]):
         for song in songs:
             self.spotify.add_to_queue(uri = song.uri, device_id = None)
         self.skip_tosong(song_uri = songs[0].uri)
 
-    def search(self, searchQuery):
-        search_results = self.spotify.search(searchQuery, 1, 0, "track")
+    def search(self, search_query: str) -> Song:
+        search_results = self.spotify.search(search_query, 1, 0, "track")
         tracks_items = search_results['tracks']['items']
         song_uri, song_name = tracks_items[0]['uri'], tracks_items[0]['name']
         image_url = tracks_items[0]['album']['images'][0]['url']
         search_song = Song(song_name=song_name, uri=song_uri, img=image_url)
         return search_song
 
-    def get_song(self, song):
+    def get_song(self, song: str) -> Song:
         if song in self.songkey_dict:
             return self.songkey_dict[song]
         return self.search(song)
 
-    def queue_newsong(self, course_name):
+    def queue_newsong(self, course_name: str):
         song = self.playlist[course_name].song_queue.popleft()
         next_song = self.playlist[course_name].song_queue.popleft()
         self.queue_songs(songs = [self.get_song(song), self.get_song(next_song)])
@@ -69,7 +81,7 @@ class SpotifyPlayer():
         self.playlist[self.course_queued].song_queue.append(song)
         self.playlist[self.course_queued].song_queue.appendleft(next_song)
 
-    def make_coursedict(self, file):
+    def make_coursedict(self, file: str) -> Dict:
         course_dict = dict()
         f = open(file, 'r')
         datalines = f.readlines()
@@ -82,11 +94,11 @@ class SpotifyPlayer():
             q = deque()
             for j in range(len(course_songs)):
                 q.append(course_songs[j])
-            course_dict[course_name] = Course(course_name=course_name, song_queue=q)
+            course_dict[course_name] = Course(course_name = course_name, song_queue = q)
         f.close()
         return course_dict
 
-    def make_songkeydict(self, file):
+    def make_songkeydict(self, file: str) -> Dict:
         songkey_dict = dict()
         f = open(file, 'r')
         datalines = f.readlines()
@@ -112,6 +124,7 @@ class SpotifyPlayer():
         file_path = 'credentials.txt'
         file = open(file_path,'r')
         cred_dict = {'username': 'None', 'client_id': 'None', 'client_secret': 'None', 'redirect_uri': 'None'}
+
         for cred in file.readlines():
             [label, key] = cred.split(' ')
             if label in cred_dict:
@@ -120,22 +133,10 @@ class SpotifyPlayer():
         self.spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,client_id=cred_dict['client_id'],
                                     client_secret=cred_dict['client_secret'],redirect_uri = cred_dict['redirect_uri'],
                                     username=cred_dict['username']))
+
         songkey_dict = self.make_songkeydict(file = 'audio/song_uri.csv')
         course_playlistfile = 'audio/playlists/rock.csv'
         course_dict = self.make_coursedict(file = course_playlistfile)
         self.playlist = course_dict
         self.songkey_dict = songkey_dict
         self.spotify_safetycheck()
-
-
-class Song():
-    def __init__(self,song_name,uri,img):
-        self.song_name = song_name
-        self.uri = uri
-        self.img = img
-
-
-class Course():
-    def __init__(self, course_name = None, song_queue = None):
-        self.course_name = course_name
-        self.song_queue = song_queue
